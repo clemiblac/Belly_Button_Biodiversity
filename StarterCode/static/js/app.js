f4fb5b64c1911f4857807a9ec3a3b7c536b3f4ac
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-07-16 15:44:46 -0400

    Setting up dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,4 @@
-/*d3.json("./samples.json", function(data) {
-    console.log(data);
-});*/
+
 
 function buildPlot(){
     fetch("./samples.json")
@@ -30,12 +28,20 @@ function buildPlot(){
 
             //test subject id
             var s_940=data.samples.slice(0,1);
-            var values_940 = s_940.map(s =>  s.sample_values.slice(0,11));
-            var labels_940 = s_940.map(s =>  s.otu_ids.slice(0,11));
-            var hover_940 = s_940.map(s =>  s.otu_labels.slice(0,11));
-            console.log("940 data");
-            console.log(values_940);
-            console.log(labels_940);
+            //hbar variables
+            var bar_values_940 = s_940.map(s =>  s.sample_values.slice(0,11));
+            var bar_labels_940 = s_940.map(s =>  s.otu_ids.slice(0,11));
+            var bar_hover_940 = s_940.map(s =>  s.otu_labels.slice(0,11));
+
+            console.log("940 Sample values");
+            console.log(bar_values_940);
+            console.log("940 OTU ID");
+            console.log(bar_labels_940);
+
+            //bubble variables
+            var bubble_values_940 = s_940.map(s =>  s.sample_values);
+            var bubble_labels_940 = s_940.map(s =>  s.otu_ids);
+            var bubble_hover_940 = s_940.map(s =>  s.otu_labels);
 
 
             //Plot Code
@@ -45,10 +51,10 @@ function buildPlot(){
             
             //  Create  trace.
             var hbar_data = [{
-                type: "bar",
-                x: values_940,
-                y: labels_940,
-                orientation:"h"
+                type: 'bar',
+                x: bar_values_940[0],
+                y: bar_labels_940[0],
+                orientation:'h'
             }];
                 
             // 7. Define our plot layout
@@ -59,8 +65,93 @@ function buildPlot(){
             };
                 
             // 8. Plot the chart to a div tag with id "bar-plot"
-            Plotly.newPlot("bar", hbar_data, layout);
-        
+            Plotly.newPlot('bar', hbar_data, layout);
+
+
+            //Bubble graph
+           
+            
+            var Bubble_d = {
+                x:bubble_labels_940[0],
+                y:bubble_values_940[0],
+                mode:'markers',
+                marker:{
+                    color:bubble_labels_940[0],
+                    size:bubble_values_940[0]
+                }
+            };
+            var Bubble=[Bubble_d];
+
+            var layout = {
+            title: 'Bubble Chart of each Sample',
+            showlegend: false,
+            height: 600,
+            width: 1000
+            };
+
+            Plotly.newPlot("bubble", Bubble,layout);
+
+
+            //Demographic info
+            console.log("Demographic info")
+          
+            var demographic_info = data.metadata[0];
+            console.log(demographic_info);
+
+
+            //Adding all the drop down options
+            select_id=data.names;
+            console.log(select_id);
+            //Select drowdown 
+            var drpdwnoption = d3.select("#selDataset");
+            //Append "option"
+            select_id.forEach((id) => {
+                var row=drpdwnoption.append("option");
+                for (var i=0; i < select_id.length; i++){
+                    console.log(select_id[i])
+                    //row.text(i)
+                }
+                
+            });
+            
+
+
+
+
+           /* function getData() {
+                var dropdownMenu = d3.select("#selData").node();
+
+                // Assign the value of the dropdown menu option to a variable
+                var dataset = dropdownMenu.value;
+                // Initialize an empty array for the country's data
+                var data = [];
+                var labels = [];
+              
+                if (dataset == 'us') {
+                    data = us;
+                    labels = us_labels;
+                    Plotly.restyle("pie", "values", [data]);
+                    Plotly.restyle("pie", "label", [labels]);
+                }
+                else if (dataset == 'uk') {
+                    data = uk;
+                    labels = uk_labels;
+                    Plotly.restyle("pie", "values", [data]);
+                    Plotly.restyle("pie", "label", [labels]);
+                }
+                else if (dataset == 'canada') {
+                    data = canada;
+                    labels = ca_labels;
+                    Plotly.restyle("pie", "values", [data]);
+                    Plotly.restyle("pie", "label", [labels]);
+                }
+                
+            }
+        */
         });
+
+
 }
+
+
 buildPlot()

3d0e46824800322ea6cfe6a6ad9626e7c0ce9cd2
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-07-16 14:26:19 -0400

    graphs displaying

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,62 +2,65 @@
     console.log(data);
 });*/
 
-fetch("./samples.json")
-    .then(function(resp){
-        return resp.json();
-    })
-    .then(function(data){
-        console.log(data)
-        //3 main parts of samples.json -- names, metadata,samples
-        console.log("samples subsection of samples.json");
-        console.log(data.samples);
+function buildPlot(){
+    fetch("./samples.json")
+        .then(function(resp){
+            return resp.json();
+        })
+        .then(function(data){
+            console.log(data)
+            //3 main parts of samples.json -- names, metadata,samples
+            console.log("samples subsection of samples.json");
+            console.log(data.samples);
+            ////////////////////////////////////////////////////////////////////////
+            //Use `sample_values` as the values for the bar chart.
+            var values = data.samples.map(s =>  s.sample_values.slice(0,11));
+            console.log("samples_values/values");
+            console.log(values);
+    
+            //Use `otu_ids` as the labels for the bar chart.
+            var labels = data.samples.map(s =>  s.otu_ids.slice(0,11));
+            console.log("otu_ids/labels");
+            console.log(labels);
 
-        //Use `sample_values` as the values for the bar chart.
-        var values = data.samples.map(s =>  s.sample_values);
-        console.log("samples_values/values");
-        console.log(values);
-   
-        //Use `otu_ids` as the labels for the bar chart.
-        var labels = data.samples.map(s =>  s.otu_ids);
-        console.log("otu_ids/labels");
-        console.log(labels);
+            //Use `otu_labels` as the hovertext for the chart.
+            var hovertext = data.samples.map(s =>  s.otu_labels.slice(0,11));
+            console.log("otu_labels/hovertext");
+            console.log(hovertext);
 
-        //Use `otu_labels` as the hovertext for the chart.
-        var hovertext = data.samples.map(s =>  s.otu_labels);
-        console.log("otu_labels/hovertext");
-        console.log(hovertext);
+            //test subject id
+            var s_940=data.samples.slice(0,1);
+            var values_940 = s_940.map(s =>  s.sample_values.slice(0,11));
+            var labels_940 = s_940.map(s =>  s.otu_ids.slice(0,11));
+            var hover_940 = s_940.map(s =>  s.otu_labels.slice(0,11));
+            console.log("940 data");
+            console.log(values_940);
+            console.log(labels_940);
 
-        //Sorting data
-        var numArray = [3, 2, 100, 4, 52, 41, 95, 1];
-        numArray.sort() // descending order
-        console.log(numArray)
 
-        // Slice the first 10 values
-        //var left = names.slice(0, 2);
-        //console.log(labels)
+            //Plot Code
+            //subject_940
 
-        /*
-
-        //Plot Code
-
-        // 5. Create your trace.
-        var trace = {
-            x: labels,
-            y: values,
-            type: "bar"
-        };
+            // Display the default plot
+            
+            //  Create  trace.
+            var hbar_data = [{
+                type: "bar",
+                x: values_940,
+                y: labels_940,
+                orientation:"h"
+            }];
+                
+            // 7. Define our plot layout
+            var layout = {
+                title: "Top 10 OTUs found in test subject",
+                xaxis: { title:"sample_values"  },
+                yaxis: { title: "otu ids"}
+            };
+                
+            // 8. Plot the chart to a div tag with id "bar-plot"
+            Plotly.newPlot("bar", hbar_data, layout);
         
-        // 6. Create the data array for our plot
-        var hbar_data = [trace];
-        
-        // 7. Define our plot layout
-        var layout = {
-            title: "Top 10 OTUs found in test subject",
-            xaxis: { title: "otu ids" },
-            yaxis: { title: "sample_values"}
-        };
-        
-        // 8. Plot the chart to a div tag with id "bar-plot"
-        Plotly.newPlot("bar", hbar_data, layout);
-        */
-    })
+        });
+}
+buildPlot()
---
+++ StarterCode/static/js/appCopy.js
@@ -0,0 +1,10 @@
+url="C:/Users/clemi/Desktop/Clementine-GWU-HW/Belly_Button_Biodiversity/StarterCode/samples.json"
+
+function buildPlot(){
+    d3.json(url).then(function(data){
+        console.log(data)
+
+
+
+    })
+}
